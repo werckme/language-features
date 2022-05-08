1b71d5ab53ea0ead1d82e4185de1feba0fdc1271
samba <samba@manjaro>
2022-05-08 15:49:14 +0200

    fixing inteface

--- src/DummyEnvironmentInspector.ts
+++ src/DummyEnvironmentInspector.ts
@@ -1,6 +1,8 @@
-import { IEnvironmentInspector, MidiDeviceInfo } from "./IEnvironmentInspector";
+import { EnvironmentType, IEnvironmentInspector, MidiDeviceInfo } from "./IEnvironmentInspector";
 
 export class DummyEnvironmentInspector implements IEnvironmentInspector{
+    environment: EnvironmentType = "local";
+    webplayerPresets?: string[];
     public async getMidiOutputDevices(): Promise<MidiDeviceInfo[]> {
         return [];
     }
